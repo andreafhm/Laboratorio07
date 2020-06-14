d36fc70aaeb5e8bdfff2ea4d2185917518502b68
afhm <ahuaynam@unsa.edu.pe>
2020-06-14 01:03:41 -0500

    Pregunta 1 Laboratorio 7

---
+++ app/src/main/java/firebase/app/laboratorio7/MainActivity.java
@@ -0,0 +1,82 @@
+package firebase.app.laboratorio7;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+        TextView gx, gy, gz, mx, my, mz, orientacion;
+        Sensor giroscopio, mangnetometro;
+        SensorManager sm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gx = (TextView) findViewById(R.id.Gx);
+        gy = (TextView) findViewById(R.id.Gy);
+        gz = (TextView) findViewById(R.id.Gz);
+
+        mx = (TextView) findViewById(R.id.Mx);
+        my = (TextView) findViewById(R.id.My);
+        mz = (TextView) findViewById(R.id.Mz);
+
+        orientacion = (TextView) findViewById(R.id.orientacion);
+
+
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mangnetometro = sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        giroscopio = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener( this,mangnetometro,SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener( this,giroscopio,SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event){
+
+        switch (event.sensor.getType()){
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                mx.setText(String.valueOf(event.values[0]));
+                my.setText(String.valueOf(event.values[1]));
+                mz.setText(String.valueOf(event.values[2]));
+
+
+                break;
+            case Sensor.TYPE_ACCELEROMETER:
+                gx.setText(String.valueOf(event.values[0]));
+                gy.setText(String.valueOf(event.values[1]));
+                gz.setText(String.valueOf(event.values[2]));
+                if(event.values[0]<-9){
+                    orientacion.setText("Orientación: Horizontal a la derecha ");
+                }else if(event.values[0]>9){
+                    orientacion.setText("Orientación: Horizontal a la izquierda ");
+                }else if(event.values[1]<-9){
+                    orientacion.setText("Orientación: Vertical invertido ");
+                }else if(event.values[1]>9){
+                    orientacion.setText("Orientación: Vertical derecho  ");
+                }else if(event.values[2]<-9){
+                    orientacion.setText("Orientación: Mirando hacia abajo  ");
+                }else if(event.values[2]>9){
+                    orientacion.setText("Orientación: Mirando hacia arriba");
+                }
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
